9844dd4a6ac630a4f8d1e0404a256428a23311b9
9Daniel929 <daniela4354@mytusd.org>
2026-02-03 19:58:53 -0800

    Update launcher.js

--- js/launcher.js
+++ js/launcher.js
@@ -1,1 +1,25 @@
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+
+const frame = document.getElementById("frame");
+const loader = document.getElementById("loader");
+
+setTimeout(() => {
+  loader.style.display = "none";
+  frame.style.display = "block";
+
+  // DEMO / PLACEHOLDER GAMES
+  if (id === "sab") {
+    frame.src = "https://example.com";
+  }
+  else if (id === "tsunami") {
+    frame.src = "https://example.com";
+  }
+  else if (id === "99nights") {
+    frame.src = "https://example.com";
+  }
+  else {
+    frame.src = "https://example.com";
+  }
+}, 2000);
 
