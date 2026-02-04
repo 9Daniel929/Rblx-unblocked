1a4e82a6f8fd05b81668e3915f98d74e78f66ba1
9Daniel929 <daniela4354@mytusd.org>
2026-02-03 19:58:36 -0800

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,1 +1,28 @@
+const games = [
+  {
+    id: "sab",
+    name: "Strongest Anime Battle",
+    image: "https://i.imgur.com/9XqQZ5X.png"
+  },
+  {
+    id: "tsunami",
+    name: "Escape Tsunami",
+    image: "https://i.imgur.com/1X5zK1M.png"
+  },
+  {
+    id: "99nights",
+    name: "99 Nights in the Forest",
+    image: "https://i.imgur.com/w1XKQ9A.png"
+  },
+  {
+    id: "obby",
+    name: "Mega Easy Obby",
+    image: "https://i.imgur.com/8WZcP8P.png"
+  },
+  {
+    id: "doors",
+    name: "DOORS",
+    image: "https://i.imgur.com/F1JtZkD.png"
+  }
+];
 
